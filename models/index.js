79a87acd171cbe335ee14d90d638171b7e1d8af3
Jack Sung <magicsung@gmail.com>
2023-08-10 16:48:58 +0800

    update production db connect info

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(`mysql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}/${process.env.RDS_DB_NAME}`, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
