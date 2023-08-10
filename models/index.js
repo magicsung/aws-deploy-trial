8bf38b201be64013d14f56e08033913259c45c22
Jack Sung <magicsung@gmail.com>
2023-08-10 16:36:28 +0800

    remove console.log

--- models/index.js
+++ models/index.js
@@ -10,7 +10,6 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-console.log(config);
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
